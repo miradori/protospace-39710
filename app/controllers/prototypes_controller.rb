f5c7ff090ab09f128016172558dad06678482ca7
miradori <miradori6164@gmail.com>
2023-11-12 21:50:27 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,65 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+    # @user =  Prototype.find(params[:id])
+    # @user = @prototype
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+    @user =  User.find(params[:id])
+    # @user = @prototype
+    # @user = @prototype
+  end
+
+  def edit
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  
+  # def set_tweet
+  #   @tweet = Tweet.find(params[:id])
+  # end
+end
